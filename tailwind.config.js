bc9a6e6493524def0ae2c21cc37067c60d496b06
rafisulthan11 <rafisulthann@gmail.com>
2026-04-23 18:47:51 +0700

    update fitur rekapitulasi pemasar

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,22 @@ export default {
         'hover:bg-indigo-700',
         'bg-yellow-500', // <-- TAMBAHKAN INI
         'hover:bg-yellow-600', // <-- TAMBAHKAN INI
+        'bg-gradient-to-r',
+        'from-purple-50',
+        'to-purple-100',
+        'border-purple-600',
+        'bg-purple-600',
+        'text-purple-700',
+        'from-emerald-50',
+        'to-emerald-100',
+        'border-emerald-600',
+        'bg-emerald-600',
+        'text-emerald-700',
+        'from-amber-50',
+        'to-amber-100',
+        'border-amber-600',
+        'bg-amber-600',
+        'text-amber-700',
     ],
     
     theme: {
