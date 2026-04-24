166d9cc303adb9434b96b44fe754b48cbc24785c
rafisulthan11 <rafisulthann@gmail.com>
2026-04-24 20:37:36 +0700

    update grafik prodiksi ikan

--- resources/views/pages/grafik/produksi-ikan.blade.php
+++ resources/views/pages/grafik/produksi-ikan.blade.php
@@ -218,14 +218,14 @@
                     </div>
                 </div>
                 @else
-                <div class="mb-6 bg-yellow-50 border-l-4 border-yellow-500 p-4 rounded">
+                <div class="mb-6 border-l-4 p-4 rounded" style="background-color: #fefce8; border-left-color: #eab308;">
                     <div class="flex items-start">
-                        <svg class="w-5 h-5 text-yellow-600 mt-0.5 mr-3 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
+                        <svg class="w-5 h-5 mt-0.5 mr-3 flex-shrink-0" style="color: #ca8a04;" fill="currentColor" viewBox="0 0 20 20">
                             <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd"/>
                         </svg>
                         <div>
-                            <h4 class="text-sm font-bold text-yellow-900 mb-1">Data Produksi Tidak Ditemukan</h4>
-                            <p class="text-sm text-yellow-800">
+                            <h4 class="text-sm font-bold mb-1" style="color: #713f12;">Data Produksi Tidak Ditemukan</h4>
+                            <p class="text-sm" style="color: #854d0e;">
                                 @if($tahun || $kecamatan || $komoditas)
                                     Tidak ada data produksi untuk filter yang dipilih 
                                     @if($tahun)<strong>(Tahun: {{ $tahun }})</strong>@endif
